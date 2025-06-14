6a9f8bddde41d8152ca3ebdbae24120f3b1a3973
Ahmed Assem <ahmedassem2610@gmail.com>
2025-06-14 23:24:07 +0300

    prepare deploy

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://blog-system-psi.vercel.app",
+    credentials: true,
+  })
+);
 
 // MongoDB Connection
 mongoose
@@ -24,7 +29,4 @@ app.get("/test", (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
